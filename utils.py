e2eb8be7cfabe41f3b0183eece0c0ec8da745f9c
stephane_nawrocki <nawrocki_stephane@yahoo.fr>
2023-06-05 11:28:50 +0200

    adding workflows and python files

---
+++ utils.py
@@ -0,0 +1,18 @@
+"""
+This scripts contains 
+data and functions useful for the other scripts
+"""
+import pickle
+
+MOVIES = [
+    "Men in Black",
+    "Inglorious Basterds",
+    "My name is Nobody",
+    "Pocahontas",
+    "The Fast and Furious (8)",
+    "Tom and Jerry",
+    "5"
+]
+
+nmf_model=...
+cos_sim_model=...
